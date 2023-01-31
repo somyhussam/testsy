86a912dc03d74f0d8e20d14d82d45f308d7d303f
somyhussam <119122356+somyhussam@users.noreply.github.com>
2023-01-31 18:00:52 +0300

    Update sython.py

--- sython.py
+++ sython.py
@@ -32,9 +32,16 @@ from checktele import *
 from telethon.tl.functions.messages import GetHistoryRequest
 import requests
 from config import *
+from telethon import eventsevents
+from telethon.tl.functions.messages import GetHistoryRequest
+from telethon.tl.functions.channels import JoinChannelRequest
+from telethon.tl.functions.messages import ImportChatInviteRequest
+import requests
+import asyncio
+from telethon import TelegramClient, events
 from telethon import events
 # -
-
+saython = ['yes']
 sython.start()
 c = requests.session()
 bot_username = '@t06bot'
@@ -65,30 +72,30 @@ async def join_channel():
     except BaseException:
         pass
 
-@sython.on(events.NewMessage(outgoing=True, pattern=r"\.تجميع المليار"))
+@sython.on(admin_cmd(pattern="تجميع المليار"))
 async def _(event):
-        await event.edit("حسنا, تأكد من انك مشترك ب قنوات الاشتراك الاجباري لتجنب الأخطأء")
-        channel_entity = await sython.get_entity(bot_username)
-        await sython.send_message('@t06bot', 'جاري التجميع بواسطة | SOMY TEAM')
+    if jepthon[0] == "yes":
+        await event.edit("**᯽︙سيتم تجميع النقاط , قبل كل شي تأكد من انك قمت بلانظمام الى القنوات الاشتراك الاجباري للبوت لعدم حدوث اخطاء**")
         channel_entity = await sython.get_entity(bot_username)
         await sython.send_message('@t06bot', '/start')
-        await asyncio.sleep(10)
+        await asyncio.sleep(5)
         msg0 = await sython.get_messages('@t06bot', limit=1)
         await msg0[0].click(2)
-        await asyncio.sleep(10)
+        await asyncio.sleep(5)
         msg1 = await sython.get_messages('@t06bot', limit=1)
         await msg1[0].click(0)
 
         chs = 1
         for i in range(100):
-
-            await asyncio.sleep(10)
+            if saython[0] == 'no':
+                break
+            await asyncio.sleep(5)
 
             list = await sython(GetHistoryRequest(peer=channel_entity, limit=1,
                                                    offset_date=None, offset_id=0, max_id=0, min_id=0, add_offset=0, hash=0))
             msgs = list.messages[0]
             if msgs.message.find('لا يوجد قنوات في الوقت الحالي , قم يتجميع النقاط بطريقه مختلفه') != -1:
-                await sython.send_message(event.chat_id, f"لايوجد قنوات  في البوت | SY")
+                await sython.send_message(event.chat_id, f"**لاتوجد قنوات للبوت**")
                 break
             url = msgs.reply_markup.rows[0].buttons[0].url
             try:
@@ -100,11 +107,14 @@ async def _(event):
                 msg2 = await sython.get_messages('@t06bot', limit=1)
                 await msg2[0].click(text='تحقق')
                 chs += 1
-                await sython.send_message(event.chat_id, f"تم الاشتراك في {chs} قناة")
+                await sython.send_message("me", f"تم الاشتراك في {chs} قناة")
             except:
-                await sython.send_message(event.chat_id, f"خطأ من المحتمل تم حظر الانضمام ")
+                await sython.send_message(event.chat_id, f"**خطأ , ممكن تبندت**")
                 break
-        await sython.send_message(event.chat_id, "تم الانتهاء من التجميع !")
+        await sython.send_message(event.chat_id, "**تم الانتهاء من التجميع !**")
+
+    else:
+        await event.edit("يجب الدفع لاستعمال هذا الامر !")
 
 
 
@@ -168,7 +178,7 @@ async def _(event):
 ☆ PING : `{ms}`
 ☆ DATE : `{m9zpi}`
 ☆ ID : `{event.sender_id}`
-☆ SOURCE SYTHON : @zbaak**
+☆ SOURCE SYTHON : @SAYTHONH**
 ''')
 
 
