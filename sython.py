358f9a6cde05f3cb5aa48d5b4539e00bf6d0ff16
somyhussam <119122356+somyhussam@users.noreply.github.com>
2023-02-01 11:33:40 +0300

    Update sython.py

--- sython.py
+++ sython.py
@@ -1,3 +1,28 @@
+import telethon
+from telethon import events
+from config import *
+import os
+import logging
+import asyncio
+import time
+from telethon.tl import functions, types
+from telethon.tl.functions.messages import ImportChatInviteRequest as Get
+from telethon.utils import get_display_name
+from telethon.tl.functions.channels import JoinChannelRequest
+from telethon.errors import FloodWaitError
+from telethon import TelegramClient, events
+import asyncio
+from collections import deque
+from telethon import functions
+from telethon.errors.rpcerrorlist import (
+    UserAlreadyParticipantError,
+    UserNotMutualContactError,
+    UserPrivacyRestrictedError,
+)
+from telethon.tl.functions.channels import InviteToChannelRequest
+from telethon.tl.types import InputPeerUser
+
+
 from telethon.errors.rpcerrorlist import YouBlockedUserError
 from telethon.tl import functions
 from hijri_converter import Gregorian
